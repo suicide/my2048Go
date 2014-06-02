dbdbff8ea5f3868493f7432a0ecfac576c55972a
suicide <suicide@get-it.us>
2014-06-02 17:51:41 +0200

    nicer rendering

--- src/my2048/game/BoardRenderer.go
+++ src/my2048/game/BoardRenderer.go
@@ -2,8 +2,19 @@ package game
 
 import "fmt"
 
+const separator = "-------------------------------------"
+
 func render(board *board, score int) {
 
-	fmt.Println(board, score)
+	fmt.Println(separator)
+
+	for x := 0; x < BoardSize; x++ {
+
+		fmt.Printf("| %6d | %6d | %6d | %6d |\n", board.Get(x, 0).Value(), board.Get(x, 1).Value(), board.Get(x, 2).Value(), board.Get(x, 3).Value())
+		fmt.Println(separator)
+
+	}
+
+	fmt.Println("Score: ", score)
 
 }
